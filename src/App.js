468987dc123f40dc7e575bfff6446774db690d20
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-09-21 23:20:48 +0600

    base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import Projects from "./views/Projects";
+import Possibilities from "./views/Possibilities";
+import Calendar from "./views/Calendar";
+import Tasks from "./views/Tasks";
 import Layout from "./components/Layout";
 
 function App() {
-  return (
-   <Layout />
-  );
+    return (
+        <Router>
+            <Switch>
+                <Layout>
+                    <Route exact path="/"><Redirect to="/projects" /></Route>
+                    <Route path="/projects"><Projects/></Route>
+                    <Route path="/tasks"><Tasks/></Route>
+                    <Route path="/calendar"><Calendar/></Route>
+                    <Route path="/possibilities"><Possibilities/></Route>
+                </Layout>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
