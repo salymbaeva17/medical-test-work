5f35b187edb36fe4ac3ed45b75753dffe973b88f
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-09-22 17:41:16 +0600

    project card

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -3,16 +3,20 @@ import "./style.css"
 
 const ProjectCard = ({el}) => {
     return (
-        <div className="col-3">
+        <div className="col-3 mb-4">
             <div className="project-card">
                 <img src={el.image} alt={el.title} />
                 <div className="project-info">
                     <h3 className="project-title">{el.title}</h3>
                     <div className="project-date">
-                        <span>{el.startDate}</span> - <span>{el.finishDate}</span>гг.
+                        <i className='bx bx-calendar-alt'/><span>{el.startDate?.split("-").join(".")}</span> - <span>{el.finishDate?.split("-").join(".")}</span>гг.
                     </div>
-                    <span className="project-author">{el.author}</span>
-                    <span className="project-company">{el.company}</span>
+                    <p className="project-author">
+                        <i className='bx bxs-user'/>{el.author}
+                    </p>
+                    <p className="project-company">
+                        <i className='bx bx-ruble'/>{el.company}
+                    </p>
                 </div>
             </div>
         </div>
