bee86d7f19a20a3c73f4d54a3ea9b9d4736d3d50
Aitolkun Salymbaeva <salymbaeva17@gmail.com>
2021-09-22 17:40:26 +0600

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import "./style.css"
-
+import avatar from "./5351552.png"
 
 const Header = () => {
     return (
         <header className="header">
-           <div className="container">
-               <div className="user__avatar">
-                   <h2>privet</h2>
-               </div>
-           </div>
+            <div className="container h-100 d-flex align-items-center">
+                <img src={avatar} className="ms-auto d-block"
+                     style={{width: "38px", height: "38px", borderRadius: "50%", objectFit: "cover",}}
+                     alt="user avatar"/>
+            </div>
         </header>
     );
 };
